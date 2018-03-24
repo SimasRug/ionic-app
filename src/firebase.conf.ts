82637366e5a35619b52415e322709953d13dd0f0
Simonas Rugevicius <simonas.rugevicius@gmail.com>
2018-03-24 21:02:28 +0100

    Delete firebase.conf.ts

--- src/firebase.conf.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyAFALUgsQCmiwuZaftNDw0YGFDXdklqSr0",
-    authDomain: "wod-recod.firebaseapp.com",
-    databaseURL: "https://wod-recod.firebaseio.com",
-    projectId: "wod-recod",
-    storageBucket: "wod-recod.appspot.com",
-    messagingSenderId: "836915039983"
-  };
